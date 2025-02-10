597c9510873e3ef6e009308780e562baeb7d09fe
DeveloperDhanush-git <dhanushlingam6363@gmail.com>
2025-02-10 14:34:33 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Button from "./Common/DynamicComponents/Button/Button"; // Import the Button component
+import Button from "./Common/DynamicComponents/Button/Button"; 
 import React from "react";
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-2xl font-bold mb-4">Dynamic Button with Hover</h1>
 
-      {/* Different buttons with hover effects */}
+    
       <Button text="Primary" color="blue" onClick={() => alert("Primary Button Clicked!")} />
       <Button text="Success" color="green" onClick={() => alert("Success Button Clicked!")} />
       <Button text="Danger" color="red" onClick={() => alert("Danger Button Clicked!")} />
