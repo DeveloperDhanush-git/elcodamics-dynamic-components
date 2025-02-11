681ee5124e06f21d68c8d581af547162c6dc5093
POOMANI M <poomani.m007@gmail.com>
2025-02-11 12:32:13 +0530

    color

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-6">
       <h1 className="text-3xl font-bold mb-6">Dynamic More Info Button</h1>
 
-      <Button text="More Info - Blue" bgColor="purple" borderColor="blue" borderRadius="xl" />
+      <Button text="More Info - Blue" bgColor="green" borderColor="blue" borderRadius="xl" />
       <Button text="More Info - Green" bgColor="green" borderColor="gray" borderRadius="full" />
       <Button text="More Info - Red" bgColor="blue" borderColor="gray" borderRadius="md" />
        </div>
